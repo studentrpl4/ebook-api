8bd55bc9ab773de163b50b9d4f7f4f29c8640daf
unknown <3103120190@student.smktelkom-pwt.sch.id.com>
2022-11-07 10:20:19 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,12 +21,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('me',[AuthController::class, 'me']);
-
+/**Route::get('me', [AuthController::class, 'me']
+);
 Route::resource('book', BookController::class)->except(
-    ['create', 'edit']
+    ['create','edit']
 );
+Route::resource('authors', AuthorController::class)->except(
+    ['create','edit']
+); */
 
-Route::resource('author', AuthorController::class)->except(
-    ['create', 'edit']
-);
+//public route
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/book', [BookController::class, 'index']);
+Route::get('/book/{id}', [BookController::class, 'show']);
+Route::get('/Authors', [AuthorController::class, 'index']);
+Route::get('/Authors/{id}', [AuthorController::class, 'show']);
+
+//protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('book', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthorController::class)->except('create', 'edit', 'show', 'index');
+});
